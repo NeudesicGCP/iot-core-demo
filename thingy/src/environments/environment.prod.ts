3e1413536d411a64526f3151bc377151d58a1071
Matthew Emes <matthew.emes@neudesic.com>
2017-10-31 16:25:51 -0700

    Fix up URL for prod builds, remove unused object from environment

--- thingy/src/environments/environment.prod.ts
+++ thingy/src/environments/environment.prod.ts
@@ -1,11 +1,6 @@
 export const environment = {
   production: true,
   debug: true,
-  googleProject: {
-    id: 'memes-sandbox',
-    locationId: 'us-central1',
-    registryId: 'memes-registry'
-  },
   registrationAuthToken: 'ocgcpDemo',
-  registrationURL: `https://registration-dot-${this.googleProject.id}.appspot.com/`
+  registrationURL: `https://registration-dot-memes-sandbox.appspot.com/`
 };
--- thingy/src/environments/environment.ts
+++ thingy/src/environments/environment.ts
@@ -6,11 +6,6 @@
 export const environment = {
   production: false,
   debug: true,
-  googleProject: {
-    id: 'memes-sandbox',
-    locationId: 'us-central1',
-    registryId: 'memes-registry'
-  },
   registrationAuthToken: 'ocgcpDemo',
-  registrationURL: `https://registration-dot-${this.googleProject.id}.appspot.com/`
+  registrationURL: `https://registration-dot-memes-sandbox.appspot.com/`
 };
